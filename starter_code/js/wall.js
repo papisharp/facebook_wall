0e6681ca36fb50b8debf4180600fc1e93e803569
Diana Salsbury <diana.salsbury@gmail.com>
2016-03-15 00:12:06 -0400

    Update wall.js

--- starter_code/js/wall.js
+++ starter_code/js/wall.js
@@ -3,4 +3,12 @@ $(document).ready(function()
 	$('#message').focus();
 
 	// YOUR CODE GOES HERE
+	
+	// 1. Make a div for the "wall post" to go in in your HTML.
+	
+	// 2. Make the text in the textbox appear in your div in html when the user clicks the post button.
+	
+	// 3. Use an if statement to only "post" if the user has actually entered text.
+	
+	// 4. Add a picture to all of the posts. 
 });
